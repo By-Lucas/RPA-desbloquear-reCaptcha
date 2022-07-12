3d60b25451f85e3b3db80cf08687b3bc69559655
by-lucas <lukasmulekezika2@gmail.com>
2022-07-12 15:28:29 -0300

    Ajuestes de erro

--- teste3.py
+++ teste3.py
@@ -1,12 +1,13 @@
 # recaptcha libraries
 import speech_recognition as sr
-import pydub
-import urllib
 # import pydub
+import pydub
+
+import urllib
 import urllib.request
 
 import random
-import os
+import os, sys
 import time
 
 from selenium.webdriver.common.keys import Keys
@@ -20,10 +21,13 @@ def delay():
     time.sleep(random.randint(2,3))
 
 options = webdriver.ChromeOptions() 
-options.add_argument("start-maximized")
+options.add_argument("start-minimized")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 driver = webdriver.Chrome(options=options, executable_path=os.getcwd()+'\\chrome_driver\\chromedriver.exe')
+#driver.get("https://web.trf3.jus.br/consultas/Internet/ConsultaReqPag")
+
+# Recaptcha teste
 driver.get("https://www.google.com/recaptcha/api2/demo")
 
 # Switch  to recaptcha frame
@@ -49,31 +53,40 @@ frames=driver.find_elements(By.TAG_NAME,"iframe")
 driver.switch_to.frame(frames[-1])
 delay()
 
-#Click on the play buttom
-driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div/button").click()
+#Information error
+erro = driver.find_element(By.CLASS_NAME, "rc-doscaptcha-header-text").text
+print('[INFO] Error: ',erro)
+if erro:
+    print('Seu computador ou sua rede podem estar enviando consultas automáticas. ')
+    driver.close()
+    sys.exit('saindo...')
+else:
 
-#get the mp3 audio file
-src = driver.find_element(By.ID,"audio-source").get_attribute("src")
-print("[INFO] Audio src: %s"%src)
+    #Click on the play buttom
+    driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div/button").click()
 
-#download the mp3 audio file from the souce
-urllib.request.urlretrieve(src, os.getcwd()+"\\sample.mp3")
-sound = pydub.AudioSegment.from_mp3(os.getcwd+"\\sample.mp3")
-sound.export(os.getcwd()+"\\sample.wav", format="wav")
-sample_audio = sr.AudioFile(os.getcwd()+"\\sample.wav")
-r = sr.Recognizer()
-with sample_audio as source:
-    audio = r.record(source)
+    #get the mp3 audio file
+    src = driver.find_element(By.ID,"audio-source").get_attribute("src")
+    print("[INFO] Audio src: %s"%src)
 
-#translat audio to text with google voice recognition
-key = r.recognize_google(audio)
-print("[INFO] Recaptcha passcode: %s" %key)
+    #download the mp3 audio file from the souce
+    urllib.request.urlretrieve(src, os.getcwd()+"\\sample.mp3")
+    sound = pydub.AudioSegment.from_mp3(os.getcwd()+"\\sample.mp3")
+    sound.export(os.getcwd()+"\\sample.wav", format="wav")
+    sample_audio = sr.AudioFile(os.getcwd()+"\\sample.wav")
+    r = sr.Recognizer()
+    with sample_audio as source:
+        audio = r.record(source)
 
-# key in results and submit
-delay()
-driver.find_element(By.ID,"audio-response").send_keys(key.lower())
-driver.find_element(By.ID,"audio-response").send_keys(Keys.ENTER)
-time.sleep(5)
-driver.switch_to.default_content()
-time.sleep(5)
-driver.find_element(By.ID,"recaptcha-demo-submit").click()
+    #translat audio to text with google voice recognition
+    key = r.recognize_google(audio)
+    print("[INFO] Recaptcha passcode: %s" %key)
+
+    # key in results and submit
+    delay()
+    driver.find_element(By.ID,"audio-response").send_keys(key.lower())
+    driver.find_element(By.ID,"audio-response").send_keys(Keys.ENTER)
+    time.sleep(5)
+    driver.switch_to.default_content()
+    time.sleep(5)
+    driver.find_element(By.ID,"recaptcha-demo-submit").click()
